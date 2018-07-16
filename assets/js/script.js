bbf32a975b9af316fc857dccbbe91b042a3d2f8d
Ryan <ryan.elr@outlook.com>
2018-07-16 15:38:14 +0200

    fontfamily

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,6 @@ $(document).ready(function(){
 var guitarApp = angular.module('guitarApp', []);
  guitarApp.controller('guitarCtrl', function($scope, $http){
    $http.get('assets/js/guitare.json').then(function(response){
-     $scope.guitars = response.data;
+     $scope.guitare = response.data;
    });
  });
