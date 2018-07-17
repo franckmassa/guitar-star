ab56d72e4e0e19ba6c51816a5707f034596374e1
Amélie Revel <amelie.revel20@yahoo.fr>
2018-07-17 12:32:16 +0200

    Ajout modal et debut panier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
-$(document).ready(function(){
-  $("#myBtn").click(function(){
-      $("#myModal").modal();
-  });
-});
-
 var guitarApp = angular.module('guitarApp', []);
- guitarApp.controller('guitarCtrl', function($scope, $http){
+ guitarApp.controller('guitarCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope){
    $http.get('assets/js/guitare.json').then(function(response){
      $scope.guitars = response.data;
    });
   $scope.changeFilter = function(newFilter){
       $scope.itemsCategories = newFilter;
   }
+  //déclaration des variables
+  $scope.quantityProduct = 1;
+  //arnaque
+  $scope.addProduct = function(){
+    $rootScope.basket = {
+      items: [{
+        name: 'Gibson SG Standard 2018 HC',
+        reference: '003',
+        price: 1088
+      }]
+    };
 });
